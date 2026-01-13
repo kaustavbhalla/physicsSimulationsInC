7f31b8653002632840b86cab060bc5d6f1c7b941
Kaustav Bhalla <kaustavbhalla15@gmail.com>
2026-01-13 10:56:54 +0000

    Completed GUI Controls for Spring Mass System

--- springMassSystem/mainFile.c
+++ springMassSystem/mainFile.c
@@ -1,4 +1,7 @@
 #include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 #include <math.h>
 #include <stdio.h>
 
@@ -6,12 +9,13 @@
 #define MASS_WIDTH 100
 #define FPS 240
 #define NUM_SPRING_ELEMENTS 16
-#define SPRING_ELEM_LENGTH 60
-#define K 10
-#define FRICTION 10
 
 float v = 80;
 float a = 2;
+float K = 10.0f;
+float FRICTION = 10.0f;
+float X_REST;
+float SPRING_ELEM_LENGTH = 60;
 
 typedef struct {
   Vector2 start, end;
@@ -45,10 +49,11 @@ void floorDraw(int HEIGHT, int WIDTH) {
   DrawLineEx(start, end, THICK, GRAY);
 }
 
-void massDraw(int HEIGHT, int WIDTH, float x_mass) {
+Rectangle massDraw(int HEIGHT, int WIDTH, float x_mass) {
   Rectangle rect = {x_mass, (HEIGHT * 0.6) - MASS_WIDTH - (float)THICK / 2,
                     MASS_WIDTH, MASS_WIDTH};
   DrawRectangleRec(rect, RED);
+  return rect;
 }
 
 int main() {
@@ -74,20 +79,68 @@ int main() {
 
     BeginDrawing();
 
+    GuiLoadStyleDefault();
+    GuiSetStyle(DEFAULT, BACKGROUND_COLOR, 0x1E1E1EFF);
+    GuiSetStyle(DEFAULT, BORDER_COLOR_NORMAL, 0x444444FF);
+    GuiSetStyle(DEFAULT, TEXT_COLOR_NORMAL, 0xE0E0E0FF);
+
+    GuiSetStyle(SLIDER, BASE_COLOR_NORMAL, 0x2A2A2AFF);
+    GuiSetStyle(SLIDER, TEXT_COLOR_NORMAL, 0xE0E0E0FF);
+    GuiSetStyle(SLIDER, BORDER_COLOR_NORMAL, 0x555555FF);
+
+    GuiSetStyle(BUTTON, BASE_COLOR_NORMAL, 0x333333FF);
+    GuiSetStyle(BUTTON, BASE_COLOR_FOCUSED, 0x444444FF);
+    GuiSetStyle(BUTTON, BASE_COLOR_PRESSED, 0x555555FF);
+
     ClearBackground(BLACK);
+
     floorDraw(HEIGHT, WIDTH);
+    springDraw(HEIGHT, WIDTH, x_mass);
+    Rectangle rect = massDraw(HEIGHT, WIDTH, x_mass);
 
     dt = GetFrameTime();
     float fric = v > 0 ? -FRICTION : FRICTION;
     a = (-K * (x_mass - X_REST)) + fric;
     v += a * dt;
     x_mass += v * dt;
-    massDraw(HEIGHT, WIDTH, x_mass);
-    springDraw(HEIGHT, WIDTH, x_mass);
-    DrawFPS(10, 10);
 
-    DrawText("Spring Mass system simulator goes here!", WIDTH / 10, 100, 30,
-             GREEN);
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+      Vector2 m = GetMousePosition();
+      if (CheckCollisionPointRec(m, rect)) {
+        x_mass = m.x;
+        v = 0;
+      }
+    }
+
+    float panelWidth = 360;
+    float panelX = WIDTH - panelWidth - 20;
+    float controlWidth = 220;
+
+    Rectangle panel = {panelX, 20, panelWidth, 280};
+
+    float controlX = panel.x + (panel.width - controlWidth) / 2.0f;
+
+    GuiPanel(panel, "Spring Controls");
+
+    GuiSliderBar((Rectangle){controlX, panel.y + 50, 220, 20}, "Stiffness",
+                 NULL, &K, 1, 100);
+
+    GuiSliderBar((Rectangle){controlX, panel.y + 90, 220, 20}, "Friction", NULL,
+                 &FRICTION, 0, 50);
+
+    GuiSliderBar((Rectangle){controlX, panel.y + 130, 220, 20}, "Rest Pos",
+                 NULL, &X_REST, WIDTH * 0.1f, WIDTH * 0.9f);
+
+    GuiSliderBar((Rectangle){controlX, panel.y + 170, 220, 20}, "Spring Size",
+                 NULL, &SPRING_ELEM_LENGTH, 20, 120);
+
+    if (GuiButton((Rectangle){controlX, panel.y + 210, 220, 30},
+                  "Reset System")) {
+      x_mass = X_REST;
+      v = 0;
+    }
+
+    DrawFPS(10, 10);
 
     EndDrawing();
   }
